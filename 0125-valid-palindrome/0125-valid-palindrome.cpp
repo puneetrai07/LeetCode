c63f985de1d21478fc87667457cf5b91e55bf130
Puneet Rai <73062696+puneetrai07@users.noreply.github.com>
2026-01-30 12:39:19 +0530

    Time: 4 ms (16.88%), Space: 10 MB (38.02%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,27 +1,21 @@
 class Solution {
-    
-private:
-    string processed(string s){
-        string temp="";
-        for(int i=0;i<s.size();i++){
-            if((s[i]>='a'&&s[i]<='z')||(s[i]>='0'&&s[i]<='9'))
-                temp.push_back(s[i]);
-            if(s[i]>='A'&&s[i]<='Z')
-                temp.push_back(s[i]-'A'+'a');
-        }
-        return temp;
-    }
-    
 public:
     bool isPalindrome(string s) {
-        string temp=processed(s);
-       // cout<<temp;
-        int st=0,end=temp.size()-1;
-        while(st<=end){
-            if(temp[st]!=temp[end])
-                return false;
-            st++;
-            end--;
+        int n=s.size();
+        int i=0, j=n-1;
+
+        while(i < j){
+            while(i<j && !isalnum(s[i]))
+            i++;
+            while(i<j && !isalnum(s[j]))
+            j--;
+
+            if(tolower(s[i]) != tolower(s[j]))
+            return false;
+
+
+            i++;
+            j--;
         }
         return true;
     }
